c95abee58ea39d069711bed69b274dc240b9d05f
Markus Matiaschek <mmatiaschek@gmail.com>
2023-01-26 14:57:53 +0100

    Add websocket server on damagames.com:3000 (#4)
    * Add websocket server on damagames.com:3000
    * Fix port settings of backend and websocket server
    * Add socket.io subfolder to traefik rule
    * Add sockets subdomain hosting
    * Fix CORS errors
    * Fix socket port
    * Add REACT frontend and backend URLs for game sharing and api access
    * Add frontend ENV to GitHub variables documentation
    * Add .env.example and copy to .env in Dockerfile
    * hardcode URLs into .env
    * Add cors path * to Laravel API TODO setup properly
    * Update backend URL wir /api path

--- frontend/src/utils/socket.io.js
+++ frontend/src/utils/socket.io.js
@@ -1,6 +1,7 @@
 // socket.io.js
 import io from "socket.io-client";
 
-const socket = io("https://dama.up.railway.app", { port: 7744 });
+//const socket = io("https://dama.up.railway.app", { port: 7744 });
+const socket = io("https://damagames.com", { port: 443 });
 
 export default socket;
