21a903e4d67a6e91a84010ed296b6c2e21f0d5e3
Tesfaye Lemi <tesfu9503@gmail.com>
2023-01-31 17:51:21 +0300

    changes to socket io port

--- frontend/src/utils/socket.io.js
+++ frontend/src/utils/socket.io.js
@@ -1,7 +1,7 @@
 // socket.io.js
 import io from "socket.io-client";
 const socket = io(process.env.REACT_APP_SOCKET_URL, {
-  port: process.env.REACT_APP_SOCKET_PORT,
+  port: 7744,
 });
 
 export default socket;
