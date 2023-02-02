45b3a360b23258aea3b21115ae71a21bcf6b66ee
kbleul <kbleulseged@gmail.com>
2023-02-02 12:57:44 +0300

    moves counter reset on rematch bug fix

--- frontend/src/utils/socket.io.js
+++ frontend/src/utils/socket.io.js
@@ -1,6 +1,6 @@
 // socket.io.js
 import io from "socket.io-client";
 
-const socket = io("172.17.104.251:7744");
+const socket = io(process.env.REACT_APP_SOCKET_URL, { port: process.env.REACT_APP_SOCKET_PORT });
 
 export default socket;
