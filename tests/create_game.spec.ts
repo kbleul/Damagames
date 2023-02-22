7cd704bcfda723d8e4345b76a51fb78486f6b768
Markus Matiaschek <mmatiaschek@gmail.com>
2023-02-22 13:46:32 +0100

    Add e2e readme and fix create_game test

--- tests/create_game.spec.ts
+++ tests/create_game.spec.ts
@@ -10,14 +10,15 @@ test('has title', async ({ page }) => {
 test('Create Game', async ({ page }) => {
     await page.goto('/');
 
-    // Wait for the "Create Game" button to appear and click it
-    const createGameButton = await page.waitForSelector('button', { text: 'Create Game' });
-    await createGameButton.click();
     // TODO: fix UI
     // somehow without a hard timeout the UI becomes flaky and the game is not created
     await page.waitForTimeout(1000);
+
+    // Wait for the "Create Game" button to appear and click it
+    const createGameButton = await page.waitForSelector('button', { text: 'Create Game' });
+    await createGameButton.click();
     // Wait for the page to navigate to the new URL
-    await page.waitForLoadState('networkidle', { timeout: 5000 });
+    //await page.waitForLoadState('networkidle', { timeout: 5000 });
 
     // Wait for the text input to appear and fill it
     const nameInput = await page.waitForSelector('input[type="text"]');
@@ -27,6 +28,6 @@ test('Create Game', async ({ page }) => {
     const createButton = await page.waitForSelector('button', { text: 'Create' });
     await createButton.click();
     // Wait for the heading to appear and assert its text
-    await page.waitForLoadState('networkidle', { timeout: 5000 });
+    //await page.waitForLoadState('networkidle', { timeout: 5000 });
     await expect(page.getByRole('heading')).toHaveText(/Great Work/);
 });
