83ccedd42ba0a04d24878c0416f55d71f69cb5af
Tesfaye Lemi <tesfu9503@gmail.com>
2023-03-01 15:56:30 +0300

    local url chaged to test url

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
     actionTimeout: 6000,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
-    baseURL: "http://localhost:3000",
+    baseURL: "https://test.damagames.com",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
