1d2699caed81e761b4b293f63e78e412afb6ca35
Tesfaye Lemi <tesfu9503@gmail.com>
2023-03-01 15:39:48 +0300

    some error fix

--- tests/create_game.spec.ts
+++ tests/create_game.spec.ts
@@ -1,16 +1,14 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test';
 
-test("has title", async ({ page }) => {
-  await page.goto("/");
+test('has title', async ({ page }) => {
+    await page.goto('/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page.getByRole("button", { name: "Create Game" })).toHaveLength(
-    5
-  );
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Dama/);
 });
 
-test("Create Game", async ({ page }) => {
-  await page.goto("/");
+test('Create Game', async ({ page }) => {
+    await page.goto('/');
 
     // TODO: fix UI
     // somehow without a hard timeout the UI becomes flaky and the game is not created
@@ -22,9 +20,9 @@ test("Create Game", async ({ page }) => {
     // Wait for the page to navigate to the new URL
     //await page.waitForLoadState('networkidle', { timeout: 5000 });
 
-  // Wait for the text input to appear and fill it
-  const nameInput = await page.waitForSelector('input[type="text"]');
-  await nameInput.fill("testrunner");
+    // Wait for the text input to appear and fill it
+    const nameInput = await page.waitForSelector('input[type="text"]');
+    await nameInput.fill('testrunner');
 
     // Wait for the "Create" button to appear and click it
     const createButton = await page.waitForSelector('button', { text: 'Create' });
