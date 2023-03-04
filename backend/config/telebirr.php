56b3928e38e975f11cd55468580b326b7cb03ae0
Amanuel Desalegn <amandesalegnb@gmail.com>
2023-03-04 09:46:29 +0300

    fixing enviroments

--- backend/config/telebirr.php
+++ backend/config/telebirr.php
@@ -8,18 +8,18 @@ return [
     |--------------------------------------------------------------------------
     |
     */
-    'app_id' => env('APP_ID'),
-    'app_key' => env('APP_KEY'),
-    'public_key' => env('PUBLIC_KEY'),
-    'short_code' => env('SHORT_CODE'),
-    'app_url' => env('TB_APP_URL'),
-    'web_url' => env('TB_WEB_URL'),
-    'sdk_url' => env('TB_SDK_URL'),
+    'app_id' => env('TELEBIRR_APP_ID'),
+    'app_key' => env('TELEBIRR_APP_KEY'),
+    'public_key' => env('TELEBIRR_PUBLIC_KEY'),
+    'short_code' => env('TELEBIRR_SHORT_CODE'),
+    'app_url' => env('TELEBIRR_TB_APP_URL'),
+    'web_url' => env('TELEBIRR_TB_WEB_URL'),
+    'sdk_url' => env('TELEBIRR_TB_SDK_URL'),
 
 
-    'notify_url' => env('NOTIFY_URL'),
-    'return_url' => env('RETURN_URL'),
+    'notify_url' => env('TELEBIRR_NOTIFY_URL'),
+    'return_url' => env('TELEBIRR_RETURN_URL'),
 
-    'subject' => env('SUBJECT'),
-    'receive_name' => env('RECIEVE_NAME'),
+    'subject' => env('TELEBIRR_SUBJECT'),
+    'receive_name' => env('TELEBIRR_RECIEVE_NAME'),
 ];
