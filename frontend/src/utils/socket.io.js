d22612c37f21e71e45a9a4225419347a88dfbb12
kbleul <kbleulseged@gmail.com>
2023-03-06 08:44:35 +0300

    fix user context updated on game win

--- frontend/src/utils/socket.io.js
+++ frontend/src/utils/socket.io.js
@@ -1,6 +1,7 @@
 // socket.io.js
 import io from "socket.io-client";
 
-const socket = io(process.env.REACT_APP_SOCKET_URL, { port: process.env.REACT_APP_SOCKET_PORT });
+//const socket = io(process.env.REACT_APP_SOCKET_URL, { port: process.env.REACT_APP_SOCKET_PORT });
 
+const socket = io("localhost:3000")
 export default socket;
