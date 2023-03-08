520a51483e4d9c367ef02c5dbb4ee1680c74066c
Amanuel Desalegn <amandesalegnb@gmail.com>
2023-03-08 14:19:59 +0300

    fix storage path

--- backend/config/filesystems.php
+++ backend/config/filesystems.php
@@ -39,7 +39,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => public_path('app/public'),
-            'url' => env('APP_URL') . '/storage',
+            'url' => env('APP_URL') . '/app/public',
             'visibility' => 'public',
             'throw' => false,
         ],
