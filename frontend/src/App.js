74af7c009cf735117e1013a96bf44c927ff5666b
kbleul <kbleulseged@gmail.com>
2023-03-08 09:23:19 +0300

    socket server connect on load fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -59,7 +59,7 @@ const App = () => {
 
   useEffect(() => {
     socket.on("connect", () => {
-      console.log("Connected to Socket.io server");
+      console.log("Connected to Socket.io server *");
     });
   });
 
