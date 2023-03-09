9bec02e7c2b5eb9d84388ddfd7dbe938c9d419c0
kbleul <kbleulseged@gmail.com>
2023-03-09 11:19:17 +0300

    changes to docker.stage file  add frontend dependency

--- frontend/src/utils/socket.io.js
+++ frontend/src/utils/socket.io.js
@@ -3,14 +3,6 @@ import io from "socket.io-client";
 
 const socket = io(process.env.REACT_APP_SOCKET_URL, { port: process.env.REACT_APP_SOCKET_PORT });
 
-
-// , {
-//     // Set the following options to prevent disconnection on external events
-//     reconnection: true,
-//     reconnectionAttempts: Infinity,
-//     reconnectionDelay: 1000,
-//     timeout: 20000,
-//     keepAlive: true
-//   }
 export default socket;
+
 
