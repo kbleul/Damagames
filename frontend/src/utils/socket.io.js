f4605b7dd6d0c2b29de3dc63c0be399e10a406dd
kbleul <kbleulseged@gmail.com>
2023-03-09 16:45:21 +0300

    add erconnection parameter to socket.io

--- frontend/src/utils/socket.io.js
+++ frontend/src/utils/socket.io.js
@@ -1,7 +1,15 @@
 // socket.io.js
 import io from "socket.io-client";
 
-const socket = io(process.env.REACT_APP_SOCKET_URL, { port: process.env.REACT_APP_SOCKET_PORT });
+const socket = io(process.env.REACT_APP_SOCKET_URL,
+    { port: process.env.REACT_APP_SOCKET_PORT },
+    {
+        reconnection: true,
+        reconnectionAttempts: Infinity,
+        reconnectionDelay: 1000,
+        reconnectionDelayMax: 5000,
+        timeout: 20000
+    });
 
 export default socket;
 
