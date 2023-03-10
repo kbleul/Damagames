f35fc039ea19ec3da0d167369fbe8abbc71e97e9
Tesfaye Lemi <tesfu9503@gmail.com>
2023-03-10 15:07:55 +0300

    error fix on public game joining and displaying their names

--- frontend/src/utils/socket.io.js
+++ frontend/src/utils/socket.io.js
@@ -1,7 +1,7 @@
 // socket.io.js
 import io from "socket.io-client";
 
-const socket = io('https://dama.up.railway.app', {
+const socket = io(process.env.REACT_APP_SOCKET_URL, {
   port: process.env.REACT_APP_SOCKET_PORT,
 });
 
