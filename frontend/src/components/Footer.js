ecaa53576a3cbd085d91efc8c90d3899e0dc73ff
kbleul <kbleulseged@gmail.com>
2023-03-11 11:51:30 +0300

    update version files added and docker files updated

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,12 +1,12 @@
 
 import { useNavigate } from "react-router-dom";
-
+import VERSION from "../version";
 export const Footer = () => {
     const navigate = useNavigate();
 
     return (
         <div className="w-full bg-inherit absolute top-[95vh] text-white text-center">
-            <a onClick={() => navigate("/privacy-policy")} className="text-xs text-gray-200 cursor-pointer">Privacy Policy |  v3.0.0</a>
+            <a onClick={() => navigate("/privacy-policy")} className="text-xs text-gray-200 cursor-pointer">Privacy Policy | {VERSION}</a>
         </div>
     )
 }
