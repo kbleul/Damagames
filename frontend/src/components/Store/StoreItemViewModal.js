995b411a64cfd54ecd253d60c4e9dda292fc2755
kbleul <kbleulseged@gmail.com>
2023-03-16 12:04:28 +0300

    update user ocal data after purchase

--- frontend/src/components/Store/StoreItemViewModal.js
+++ frontend/src/components/Store/StoreItemViewModal.js
@@ -30,7 +30,7 @@ const StoreItemView = ({
 	const purchaseMutation = useMutation(
 		async (newData) =>
 			await axios.post(
-				`${process.env.REACT_APP_BACKEND_URL}purchase-item`,
+				`${process.env.STAGE_REACT_APP_BACKEND_URL}purchase-item`,
 				newData,
 				{
 					headers,
