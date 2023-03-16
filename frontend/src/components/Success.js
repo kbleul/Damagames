c8565f72626cfa24b7d68ac6e366ee48fa83c213
Tesfaye Lemi <tesfu9503@gmail.com>
2023-03-16 16:51:29 +0300

    successpage added

--- frontend/src/components/Success.js
+++ frontend/src/components/Success.js
@@ -2,8 +2,50 @@ import React from "react";
 import { FaMoneyBillWave } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import money from "../assets/money.svg";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useAuth } from "../context/auth";
 const Success = () => {
   const navigate = useNavigate();
+  const { user, token, setUser } = useAuth();
+  const headers = {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    Authorization: `Bearer ${token}`,
+  };
+
+  const profileData = useQuery(
+    ["profileDataApi"],
+    async () =>
+      await axios.get(`${process.env.REACT_APP_BACKEND_URL}profile`, {
+        headers,
+      }),
+    {
+      keepPreviousData: true,
+      refetchOnWindowFocus: false,
+      retry: false,
+      enabled: !!token,
+      onSuccess: (res) => {
+        console.log(res?.data?.data);
+        localStorage.setItem(
+          "dama_user_data",
+          JSON.stringify({
+            token,
+            user: {
+              ...user,
+              coin: res?.data?.data?.coin,
+              current_point: res?.data?.data?.current_point,
+            },
+          })
+        );
+        setUser({
+          ...user,
+          coin: res?.data?.data?.coin,
+          current_point: res?.data?.data?.current_point,
+        });
+      },
+    }
+  );
   return (
     <div className="flex flex-col space-y-2 items-center justify-center max-w-xl mx-auto p-3">
       <img src={money} />
