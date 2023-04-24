797899e5a3eb071f5bd5d7355648758d909e5350
kbleul <kbleulseged@gmail.com>
2023-04-24 15:17:47 +0300

    added settings to service worker

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -32,5 +32,5 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register(swConfig);
+serviceWorkerRegistration.register(swConfig, { mode: "standalone", prefersRelatedApplications: "true", });
 
