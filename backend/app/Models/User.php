9bbcc7e1a8f1be140f4648ce6e8ed49907f5677a
Amanuel Desalegn <amandesalegnb@gmail.com>
2023-05-17 19:25:27 +0300

    working on points

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -25,7 +25,7 @@ class User extends Authenticatable
      */
     protected $guarded = [];
 
-    public $appends = ['rank', 'coin', 'game_point', 'match_history'];
+    public $appends = ['rank', 'rank_by_point', 'coin', 'game_point', 'match_history'];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -45,6 +45,31 @@ class User extends Authenticatable
     {
         $collection = collect(User::orderByDesc('current_point')
             ->get());
+
+        $data = $collection->where('id', $this->id);
+
+        if ($data->count() > 0) {
+            return $data->keys()->first() + 1;
+        } else {
+            return 0;
+        }
+    }
+    public function getRankByPointAttribute()
+    {
+        $users = User::all()->map(function ($user) {
+            $wins = Game::where('playerOne', $this->id)
+                ->orWhere('playerTwo', $this->id)
+                ->withCount(['scores' => function ($query) {
+                    $query->where('winner', $this->id);
+                }])
+                ->get()
+                ->sum('scores_count');
+
+            $user->game_point = ($wins * CoinSetting::first()->winnerCoins) + ($wins * CoinSetting::first()->drawCoins);
+            return $user;
+        });
+
+        $collection = collect($users);
 
         $data = $collection->where('id', $this->id);
 
@@ -178,6 +203,7 @@ class User extends Authenticatable
             $user->game_point = ($wins * CoinSetting::first()->winnerCoins) + ($wins * CoinSetting::first()->drawCoins);
             return $user;
         });
+
         $collection = collect($users);
 
         $data = $collection->where('id', $id);
