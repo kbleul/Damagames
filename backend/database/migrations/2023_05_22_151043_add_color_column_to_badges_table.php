292be2fd5d0425119944a8af6a968ab46b4c712f
Amanuel Desalegn <amandesalegnb@gmail.com>
2023-05-22 18:15:45 +0300

    adding color to badges

---
+++ backend/database/migrations/2023_05_22_151043_add_color_column_to_badges_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('badges', function (Blueprint $table) {
+            $table->json('color')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('badges', function (Blueprint $table) {
+            $table->dropColumn('color');
+        });
+    }
+};
