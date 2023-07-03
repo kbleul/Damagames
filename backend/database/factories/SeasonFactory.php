d3a8d48300bb9422b5c2bee362bac11966a27139
molla-12 <mollaman19@gmail.com>
2023-07-03 10:25:40 +0300

    season seeder  update and refactor season model

--- backend/database/factories/SeasonFactory.php
+++ backend/database/factories/SeasonFactory.php
@@ -34,7 +34,7 @@ class SeasonFactory extends Factory
         $start_date = [['english' => '2023-06-15', 'amharic' => '2015-06-8'], ['english' => '2023-06-13', 'amharic' => '2015-07-8']];
         $end_date = [['english' => '2023-07-15', 'amharic' => '2015-07-8'], ['english' => '2023-04-15', 'amharic' => '2015-01-8']];
         $starting_time = [['english' => '12:00:00', 'amharic' => '09:08:00'], ['english' => '12:30:00', 'amharic' => '11:30:00']];
-        $ending_time = [['english' => '06:00:00', 'amharic' => '03:08:00'], 'english' => '06:30:00', 'amharic' => '05:30:00'];
+        $ending_time = [['english' => '06:00:00', 'amharic' => '03:08:00'], ['english' => '06:30:00', 'amharic' => '05:30:00']];
 
         return [
             'league_id' => $this->faker->randomElement($leagueName),
@@ -43,6 +43,9 @@ class SeasonFactory extends Factory
             'ending_date' => $this->faker->randomElement($end_date),
             'starting_time' => $this->faker->randomElement($starting_time),
             'ending_time' => $this->faker->randomElement($ending_time),
+            'is_active'=>0,
+            'playing_day'=>"[\"Monday\",\"Friday\"]",
+            'coin_amount'=>100,
         ];
     }
 }
