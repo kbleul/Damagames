e5018d1dbdbd7bd93c8cf652e259af60861fea40
molla-12 <mollaman19@gmail.com>
2023-07-04 14:47:53 +0300

    order prize by level

--- backend/app/Models/Season.php
+++ backend/app/Models/Season.php
@@ -55,7 +55,7 @@ class Season extends Model
 
     public function getPrizesAttribute()
     {
-        return $this->prizes()->get();
+        return $this->prizes()->orderBy('level')->get();
     }
 
     public function getIsGameTimeAttribute()
